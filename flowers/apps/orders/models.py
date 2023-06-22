7866099609d7288284c5b1352c4eb563b21b6eee
Maxim Logvinov <vvglvv2@gmail.com>
2023-06-22 17:50:56 +0300

    First commit with setting up the entire project and adding models to it

---
+++ flowers/apps/orders/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from apps.products.models import Products
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField('Дата создания', auto_now=True, max_length=0)
+    description = models.CharField('Описание', blank=True, null=True, max_length=300)
+    deliv_address = models.CharField('Адрес доставки', blank=False, max_length=150)
+    paid = models.BooleanField('Оплачено', default=False)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return '{} {} {} {} {}'.format(self.id, self.description, self.deliv_address, self.created, self.paid)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.linkorder.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='linkorder', on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, related_name='Товар', on_delete=models.CASCADE)
+    price = models.DecimalField('Цена', max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField('Количество', default=1)
+
+    def __str__(self):
+        return '{} {} {}шт {}₽'.format(self.product, self.product.title, self.quantity, self.price)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
+    class Meta:
+        verbose_name = 'Предметы заказа'
+        verbose_name_plural = 'Предметы заказов'
