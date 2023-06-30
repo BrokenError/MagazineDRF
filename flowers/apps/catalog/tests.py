617134f91f2a847a6eccfcbaab33d8154eefcae3
Maxim Logvinov <vvglvv2@gmail.com>
2023-06-30 12:42:00 +0300

    Added tests for app catalog

---
+++ flowers/apps/catalog/tests.py
@@ -0,0 +1,76 @@
+import logging
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+
+from apps.catalog.models import Categories
+from apps.catalog.services import show_categories, search_magazine
+from apps.products.models import Products
+from apps.products.services import rate_the_product
+from apps.products.tests import write_text, time_of_function
+
+logging.basicConfig(level=51, format="Logs: %(message)s")
+
+
+class TestCatalog(APITestCase):
+    """ Тестирование каталога с товарами """
+
+    @classmethod
+    def setUp(cls):
+        cls.default_time = '2000-01-01 00:00:00+03'
+        User.objects.create_superuser('root', 'vvv@gmail.com', '1111')
+        cls.user = User.objects.first()
+        cls.client = APIClient()
+
+    @classmethod
+    def tearDown(cls):
+        logging.log(51, 'Success')
+
+    def create_check_categories(self):
+        data_categories = {1: ['Букеты', 'bukety'], 2: ['Цветы', 'cveti'],
+                           3: ['События', 'sobitiya'], 4: ['Другое', 'drygoe']}
+        for _ in data_categories:
+            Categories(id=_, title=data_categories[_][0], slug=data_categories[_][1], date=self.default_time).save()
+
+    def create_category(self):
+        self.category = Categories(1, 'Букеты', 'byketi', self.default_time)
+        self.category.save()
+
+    def create_product(self):
+        self.create_category()
+        self.product = Products(1, 'ТЕСТ1', 'test', '2400', '----', '', True,
+                                self.default_time, self.default_time, self.category.id)
+        self.product.save()
+
+    @time_of_function
+    def test_search_page(self):
+        write_text('Проверка-поиск-товаров-в-каталоге')
+        self.create_product()
+        response = self.client.get('/catalog/search/', {}, format='json')
+        print(response)
+        response = self.client.post('/catalog/search/', {'prod_title_search': 'тес'}, format='json')
+        print(response)
+        logging.log(51, response.json())
+
+    @time_of_function
+    def test_catalog_main(self):
+        write_text('Проверка-все-категории')
+        self.create_product()
+        response = self.client.get('/catalog/', {}, format='json')
+        print(response)
+        logging.log(51, response.json())
+
+    @time_of_function
+    def test_show_category(self):
+        write_text('Проверка-выбранная категория')
+        self.create_product()
+        self.create_check_categories()
+        response = self.client.get('/catalog/bukety/', {}, format='json')
+        print(response)
+        logging.log(51, Categories.objects.all().values('title', 'slug'))
+        logging.log(51, response.json())
+
+
+def write_categories(cat_title):
+    return print('---- Категория {}'.format(cat_title))
--- flowers/apps/products/tests.py
+++ flowers/apps/products/tests.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory, APITestCase, APIClient
 
 from apps.catalog.models import Categories
-from apps.products.models import Products, Comments
+from apps.products.models import Products, Comments, Reviews, Rating
 from apps.products.serializers import ReviewSerializer, CommentSerializer
 from apps.products.services import base_content
 
@@ -24,36 +24,41 @@ def time_of_function(function):
 class TestProducts(APITestCase):
     """ Тестирование приложения с товарами"""
 
+    @classmethod
     def tearDown(cls):
         logging.log(51, 'Success')
 
     @classmethod
-    def setUpTestData(cls):
+    def setUp(cls):
         factory = APIRequestFactory()
         client = APIClient()
-        User.objects.create_superuser('root', 'vvv@gmail.com', '1111')
         cls.default_time = '2000-01-01 00:00:00+03'
         cls.comment = Comments.objects.first()
-        request = factory.get('/')
-        user = User.objects.first()
-        client.force_authenticate(user=user)
-        cls.user = user
         cls.factory = factory
         cls.client = client
 
+    def create_superuser(self):
+        User.objects.all().delete()
+        user = User.objects.create_superuser('root', 'vvv@gmail.com', '1111')
+        self.client.force_authenticate(user=user)
+        return user
+
     def create_category(self):
-        self.category = Categories(1, 'Букеты', 'byketi', self.default_time)
-        self.category.save()
+        Categories(1, 'Букеты', 'byketi', self.default_time).save()
+        self.category = Categories.objects.first()
+        return self.category
 
     def create_product(self):
         self.create_category()
-        self.product = Products(1, 'ТЕСТ1', 'test', '2400', '----', '', True,
-                                self.default_time, self.default_time, self.category.id)
-        self.product.save()
+        Products(1, 'ТЕСТ1', 'test', '2400', '----', '', True, self.default_time, self.default_time,
+                 self.category.id).save()
+        self.product = Products.objects.first()
+        return self.product
 
     def add_comment(self):
-        self.create_product()
-        name_form_data = {'text': 'TEST', 'user': '1', 'product': '1'}
+        product = self.create_product()
+        user = self.create_superuser()
+        name_form_data = {'text': 'TEST', 'user': user.id, 'product': product.id}
         serializer = CommentSerializer(data=name_form_data)
         valid = serializer.is_valid(raise_exception=True)
         if valid:
@@ -62,8 +67,9 @@ class TestProducts(APITestCase):
             return valid
 
     def add_review(self):
-        self.create_product()
-        name_form_data = {'name': 'TESTED', 'text': 'TEST', 'user': '1', 'product': '1'}
+        product = self.create_product()
+        user = self.create_superuser()
+        name_form_data = {'name': 'TESTED', 'text': 'TEST', 'user': user.id, 'product': product.id}
         serializer = ReviewSerializer(data=name_form_data)
         valid = serializer.is_valid(raise_exception=True)
         if valid:
@@ -75,7 +81,8 @@ class TestProducts(APITestCase):
     def test_base_content(self):
         write_text(f"Проверка-базового-контента")
         self.create_product()
-        content = base_content({'prod_slug': 'test'}, self.user.id)
+        user = self.create_superuser()
+        content = base_content({'prod_slug': 'test'}, user.id)
         print(content)
 
     @time_of_function
@@ -104,6 +111,7 @@ class TestProducts(APITestCase):
     def test_reviews_view(self):
         write_text(f"Проверка-страницы-Отзывов")
         self.add_review()
+        self.user = self.create_superuser()
         response = self.client.get('/reviews/test/')
         print(response)
         logging.log(51, response.json()['content'])
@@ -112,58 +120,65 @@ class TestProducts(APITestCase):
     def test_comments_view(self):
         write_text(f"Проверка-страницы-Комментариев")
         self.add_comment()
+        self.user = self.create_superuser()
         response = self.client.get(f'/comments/test/')
         print(response)
         logging.log(51, response.json()['content'])
 
     @time_of_function
     def test_add_reviews(self):
+        self.user = self.create_superuser()
         self.client.force_authenticate(user=self.user)
         write_text(f"Проверка-добавления-отзыва")
-        self.create_product()
-        response = self.client.post('/add-review/1', {"user": "1", "product": "1", 'name': 'test', 'text': 'success'},
-                                    format='json')
+        product = self.create_product()
+        response = self.client.post(f'/add-review/{product.id}', {"user": "1", "product": product.id,
+                                                                  'name': 'test', 'text': 'success'}, format='json')
         print(response)
         logging.log(51, response.json())
 
     @time_of_function
     def test_add_comments(self):
+        self.user = self.create_superuser()
         self.client.force_authenticate(user=self.user)
         write_text(f"Проверка-добавления-комментария")
-        self.create_product()
-        response = self.client.post('/add-comment/1', {"user": "1", "product": "1", 'text': 'success'}, format='json')
+        product = self.create_product()
+        response = self.client.post(f'/add-comment/{product.id}', {"user": "1", "product": "1", 'text': 'success'}, format='json')
         print(response)
         logging.log(51, response.json())
 
     @time_of_function
     def test_change_reviews(self):
+        self.user = self.create_superuser()
         self.client.force_authenticate(user=self.user)
         write_text(f"Проверка-изменения-отзыва")
         review = self.add_review()
-        response = self.client.post(f'/change-review/1/{review.id}', {"name": "success", "text": "test"}, format='json')
+        response = self.client.post(f'/change-review/{self.product.id}/{review.id}', {"name": "success", "text": "test"}, format='json')
         print(response)
         logging.log(51, response.json()['result'])
 
     @time_of_function
     def test_change_comments(self):
+        self.user = self.create_superuser()
         self.client.force_authenticate(user=self.user)
         write_text(f"Проверка-изменения-комментария")
         comment = self.add_comment()
-        response = self.client.post(f'/change-comment/1/{comment.id}', {"text": "success"}, format='json')
+        response = self.client.post(f'/change-comment/{self.product.id}/{comment.id}', {"text": "success"}, format='json')
         print(response)
         logging.log(51, response.json()['result'])
 
     @time_of_function
     def test_remove_review(self):
+        self.user = self.create_superuser()
         self.client.force_authenticate(user=self.user)
         write_text(f"Проверка-удаление-отзыва")
         review = self.add_review()
-        response = self.client.post(f'/delete-review/{review.id}', format='json')
+        response = self.client.post(f'/delete-review/{review.id}', {'user': self.user.id}, format='json')
         print(response)
         logging.log(51, response.json())
 
     @time_of_function
     def test_remove_comment(self):
+        self.user = self.create_superuser()
         self.client.force_authenticate(user=self.user)
         write_text(f"Проверка-удаление-комментария")
         comment = self.add_comment()
@@ -174,9 +189,10 @@ class TestProducts(APITestCase):
     @time_of_function
     def test_star(self):
         write_text(f"Проверка-оценка-товара")
+        self.user = self.create_superuser()
+        self.product = self.create_product()
         self.client.force_authenticate(user=self.user)
-        self.create_product()
-        response = self.client.post('/give-grade/1', {"grade": 5}, format='json')
+        response = self.client.post(f'/give-grade/{self.product.id}', {"grade": 5}, format='json')
         print(response)
         logging.log(51, response.json())
 
